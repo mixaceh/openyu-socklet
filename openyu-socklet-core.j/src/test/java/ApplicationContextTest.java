162a7563ce07afec948b6e1c8476c3824aa92b1a
mixaceh <mixaceh@gmail.com>
2015-09-28 22:05:59 +0800

    add usedMemory

--- openyu-socklet-core.j/src/test/java/ApplicationContextTest.java
+++ openyu-socklet-core.j/src/test/java/ApplicationContextTest.java
@@ -2,15 +2,20 @@ import org.junit.BeforeClass;
 import org.junit.Test;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 import org.openyu.commons.junit.supporter.BaseTestSupporter;
+import org.openyu.commons.lang.RuntimeHelper;
 
 public class ApplicationContextTest extends BaseTestSupporter {
 
 	@BeforeClass
 	public static void setUpBeforeClass() throws Exception {
 		begTime = System.currentTimeMillis();
-		applicationContext = new ClassPathXmlApplicationContext(
-				new String[] { "applicationContext.xml" });
+		// 計算所耗費的記憶體(bytes)
+		RuntimeHelper.gc();
+		// 原本的記憶體
+		long memory = RuntimeHelper.usedMemory();
+		applicationContext = new ClassPathXmlApplicationContext(new String[] { "applicationContext.xml" });
 		endTime = System.currentTimeMillis();
+		usedMemory = Math.max(usedMemory, (RuntimeHelper.usedMemory() - memory));
 	}
 
 	@Test
